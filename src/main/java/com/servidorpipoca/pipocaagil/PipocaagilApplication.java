437e4fa1972785208a9da3b8a51c694a3012ccc6
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-05-26 12:14:08 -0300

    commit inicial

---
+++ src/main/java/com/servidorpipoca/pipocaagil/PipocaagilApplication.java
@@ -0,0 +1,13 @@
+package com.servidorpipoca.pipocaagil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipocaagilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipocaagilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servidorpipoca/pipocaagil/PipocaagilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servidorpipoca.pipocaagil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipocaagilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
