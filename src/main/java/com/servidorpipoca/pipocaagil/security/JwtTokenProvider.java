3f3de0f6c144fa452545016e1645a81e48e23602
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-06-16 15:31:40 -0300

    jwt token provider

---
+++ src/main/java/com/servidorpipoca/pipocaagil/security/JwtTokenProvider.java
@@ -0,0 +1,40 @@
+package com.servidorpipoca.pipocaagil.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long validityInMilliseconds;
+
+    public void addTokenToResponse(Authentication authentication, HttpServletResponse response) {
+        String token = generateToken(authentication);
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+
+    public String generateToken(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+}
