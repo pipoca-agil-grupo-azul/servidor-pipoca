b66dd735ccab56b218a2f31d7d7928ed20c8a249
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-06-16 15:32:30 -0300

    SecurityConfig jwt/Bcrypt

---
+++ src/main/java/com/servidorpipoca/pipocaagil/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.servidorpipoca.pipocaagil.config;
+
+import com.servidorpipoca.pipocaagil.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    public JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((crsf) -> crsf.disable()).cors((cors) -> cors.disable())
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        return http.build();
+    }
+}
