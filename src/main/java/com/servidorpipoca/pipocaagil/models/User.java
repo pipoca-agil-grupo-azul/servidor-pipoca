8d62392fd666f9cf6ec5417ab7526a7c59c9151d
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-06-16 15:32:54 -0300

    regex password/email

--- src/main/java/com/servidorpipoca/pipocaagil/models/User.java
+++ src/main/java/com/servidorpipoca/pipocaagil/models/User.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -22,17 +26,19 @@ public class User {
 
     @Column(name = "name", length = 100, nullable = false)
     @Size(min = 8, max = 100)
+    @Pattern(regexp = "^[a-zA-Z]+(\\s+[a-zA-Z]+)+$", message = "Nome com formato inválido, precisa conter nome e sobrenome")
     @NotBlank
     private String name;
 
     @Column(name = "password", length = 60, nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Size(min = 8, max = 60)
+    @Pattern(regexp = "^(?=.*[!@#$%^&*(),.?\":{}|<>])(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "Senha inválida, precisa conter 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caractér especial")
     @NotBlank
     private String password;
 
     @Column(name = "email", length = 100, nullable = false, unique = true)
-    @Email
+    @Email(message = "Email inválido", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     @NotBlank
     private String email;
 
@@ -41,4 +47,8 @@ public class User {
     @Past
     private LocalDate dateBirth;
 
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
 }
